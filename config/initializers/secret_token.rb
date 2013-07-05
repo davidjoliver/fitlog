bf7cd58c82bfd21c61dabaed2225e2ae721ecae7
David Oliver <david.oliver@codehabit.net>
2013-07-05 08:25:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fitlog::Application.config.secret_key_base = '01a048b0b8ceac888b1aa69fc54579287860f24b6c1d91cd497b215edf6abb3172bd0c434a3343bcd0b96f379fb5d8119ef102b6c1aed7463f427397b77c39a3'
